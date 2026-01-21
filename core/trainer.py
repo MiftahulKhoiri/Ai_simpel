3bdd444d7c763aae670eeb8f1c00418802f65959
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-21 21:02:16 +0800

    Update trainer.py

--- core/trainer.py
+++ core/trainer.py
@@ -1,23 +1,35 @@
 import json
 import pickle
+from pathlib import Path
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-DATA_PATH = "data/qa.json"
-MODEL_PATH = "model/vectorizer.pkl"
+from core.utils import preprocess_text
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data" / "qa.json"
+MODEL_PATH = BASE_DIR / "model" / "vectorizer.pkl"
+
 
 def train():
     with open(DATA_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    questions = [item["question"] for item in data]
+    # PREPROCESS DATASET
+    questions = [
+        preprocess_text(item["question"])
+        for item in data
+    ]
 
     vectorizer = TfidfVectorizer()
     vectorizer.fit(questions)
 
+    MODEL_PATH.parent.mkdir(exist_ok=True)
+
     with open(MODEL_PATH, "wb") as f:
         pickle.dump(vectorizer, f)
 
-    print("Model NLP berhasil dilatih dan disimpan.")
+    print("Model NLP (dengan stemming) berhasil dilatih.")
+
 
 if __name__ == "__main__":
     train()
