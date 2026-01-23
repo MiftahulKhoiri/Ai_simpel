77078fa6281535d7a74388d26341dfafc237b8be
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-01-23 12:27:57 +0800

    Update data_manager.py

--- core/data_manager.py
+++ core/data_manager.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from core.backup import backup_qa
+from core.backup import backup_qa_once
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "qa.json"
@@ -14,7 +14,7 @@ def load_data():
 
 
 def save_data(data):
-    backup_qa()
+    backup_qa_once()
     with open(DATA_PATH, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
